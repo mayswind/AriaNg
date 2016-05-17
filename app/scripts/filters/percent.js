caf1fda1987844bb2f8cbd3b8bdb0c8ac55447c7
MaysWind <i@mayswind.net>
2016-05-17 21:52:59 +0800

    fix the value of completed percent is wrong

---
+++ app/scripts/filters/percent.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular.module("ariaNg").filter('percent', ['numberFilter', function (numberFilter) {
+        return function (value, precision) {
+            var ratio = Math.pow(10, precision);
+            var result = parseInt(value * ratio) / ratio;
+
+            return numberFilter(result, precision);
+        }
+    }]);
+})();
