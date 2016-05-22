57827fb0cff087f3229b64f770b4dc3542905b7c
MaysWind <i@mayswind.net>
2016-05-22 16:29:32 +0800

    fix scorllbar bug

--- app/scripts/core/__fix.js
+++ app/scripts/core/__fix.js
@@ -3,29 +3,10 @@
 
     //copy from AdminLTE app.js
     var fixContentWrapperHeight = function () {
+        var windowHeight = $(window).height();
         var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
-        var window_height = $(window).height();
-        var sidebar_height = $(".sidebar").height();
 
-        if ($("body").hasClass("fixed")) {
-            $(".content-wrapper, .right-side").css('height', window_height - $('.main-footer').outerHeight());
-        } else {
-            var postSetWidth;
-            if (window_height >= sidebar_height) {
-                $(".content-wrapper, .right-side").css('height', window_height - neg);
-                postSetWidth = window_height - neg;
-            } else {
-                $(".content-wrapper, .right-side").css('height', sidebar_height);
-                postSetWidth = sidebar_height;
-            }
-
-            //Fix for the control sidebar height
-            var controlSidebar = $($.AdminLTE.options.controlSidebarOptions.selector);
-            if (typeof controlSidebar !== "undefined") {
-                if (controlSidebar.height() > postSetWidth)
-                    $(".content-wrapper, .right-side").css('height', controlSidebar.height());
-            }
-        }
+        $(".content-body").css('height', windowHeight - neg);
     };
 
     $(window, ".wrapper").resize(function () {
