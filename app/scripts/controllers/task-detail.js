bee3e2af94f2615cf63774ca645257e1e0a9e42c
MaysWind <i@mayswind.net>
2016-05-24 23:30:33 +0800

    support displaying bt peers in task detail page

--- app/scripts/controllers/task-detail.js
+++ app/scripts/controllers/task-detail.js
@@ -4,11 +4,27 @@
     angular.module('ariaNg').controller('TaskDetailController', ['$scope', '$routeParams', '$interval', 'aria2RpcService', 'ariaNgSettingService', 'utils', function ($scope, $routeParams, $interval, aria2RpcService, ariaNgSettingService, utils) {
         var downloadTaskRefreshPromise = null;
 
+        var refreshPeers = function () {
+            return aria2RpcService.getPeers({
+                params: [$routeParams.gid],
+                callback: function (result) {
+                    if (!utils.extendArray(result, $scope.peers, 'peerId')) {
+                        $scope.peers = result;
+                    }
+                }
+            })
+        };
+
         var refreshDownloadTask = function () {
             return aria2RpcService.tellStatus({
                 params: [$routeParams.gid],
                 callback: function (result) {
                     var task = utils.processDownloadTask(result);
+
+                    if (task.status == 'active' && task.bittorrent) {
+                        refreshPeers();
+                    }
+
                     $scope.task = utils.copyObjectTo(task, $scope.task);
                 }
             })
