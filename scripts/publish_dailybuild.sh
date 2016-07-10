d3583b302abde6655a533bab1121afe449150dfd
MaysWind <i@mayswind.net>
2016-07-10 19:02:25 +0800

    add ci config

---
+++ scripts/publish_dailybuild.sh
@@ -0,0 +1,14 @@
+if [ $CI == "true" ] && [ $CIRCLE_BRANCH == "master" ]; then
+  cp dist $CIRCLE_ARTIFACTS/ -r;
+  git checkout -b gh-pages remotes/origin/gh-pages;
+  rm -rf css;
+  rm -rf fonts;
+  rm -rf js;
+  rm -rf langs;
+  rm -rf imgs;
+  rm -rf views;
+  rm -f index.html;
+  cp $CIRCLE_ARTIFACTS/dist/* ./ -r;
+  git commit -a -m "daily build #$CIRCLE_SHA1" --author="CI <i@mayswind.net>";
+  git push origin gh-pages;
+fi
