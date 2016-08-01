614aaee0f40f2addc3fe528e70bfd115dfd9c61b
MaysWind <i@mayswind.net>
2016-08-02 00:11:11 +0800

    update publish scripts

--- scripts/publish_dailybuild.sh
+++ scripts/publish_dailybuild.sh
@@ -12,20 +12,22 @@ if [ $CI == "true" ] && [ $CIRCLE_BRANCH == "master" ]; then
   rm -rf js;
   rm -rf langs;
   rm -rf imgs;
-  rm -rf views;
   rm -f index.html;
   rm -f README.md;
   rm -f LICENSE;
 
   cp $CIRCLE_ARTIFACTS/dist/* ./ -r;
-  cp $CIRCLE_ARTIFACTS/README.md ./
-  cp $CIRCLE_ARTIFACTS/LICENSE ./
+  cp $CIRCLE_ARTIFACTS/README.md ./;
+  cp $CIRCLE_ARTIFACTS/LICENSE ./;
 
   git add -A;
   git commit -a -m "daily build #$CIRCLE_SHA1";
   git push origin gh-pages;
 
-  zip $CIRCLE_ARTIFACTS/dist.zip $CIRCLE_ARTIFACTS/dist/* -r;
+  cd $CIRCLE_ARTIFACTS/dist/;
+  zip $CIRCLE_ARTIFACTS/dist.zip * -r -9;
+  cd ..;
+
   rm -rf $CIRCLE_ARTIFACTS/dist;
   rm -f $CIRCLE_ARTIFACTS/README.md;
   rm -f $CIRCLE_ARTIFACTS/LICENSE;
