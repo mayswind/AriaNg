35f7ae11771d961d4b6cc787eb6c578fbe000044
MaysWind <i@mayswind.net>
2016-11-06 15:23:08 +0800

    code refactor

---
+++ src/scripts/config/fileTypes.js
@@ -0,0 +1,102 @@
+(function () {
+    'use strict';
+
+    angular.module('ariaNg').constant('ariaNgFileTypes', {
+        video: [
+            '.3gp',
+            '.3gpp',
+            '.asf',
+            '.asx',
+            '.avi',
+            '.dat',
+            '.flv',
+            '.m4v',
+            '.mkv',
+            '.mov',
+            '.mp4',
+            '.mpe',
+            '.mpeg',
+            '.mpg',
+            '.rm',
+            '.rmvb',
+            '.vob',
+            '.wmv'
+        ],
+        audio: [
+            '.aac',
+            '.amr',
+            '.ape',
+            '.flac',
+            '.m4a',
+            '.mid',
+            '.mp3',
+            '.ogg',
+            '.ra',
+            '.vqf',
+            '.wav',
+            '.wma'
+        ],
+        picture: [
+            '.bmp',
+            '.emf',
+            '.gif',
+            '.jp2',
+            '.jpeg',
+            '.jpg',
+            '.png',
+            '.psd',
+            '.raw',
+            '.tga',
+            '.tif',
+            '.tiff',
+            '.wmf'
+        ],
+        document: [
+            '.doc',
+            '.docx',
+            '.key',
+            '.numbers',
+            '.odp',
+            '.ods',
+            '.odt',
+            '.pages',
+            '.pdf',
+            '.ppt',
+            '.pptx',
+            '.rtf',
+            '.txt',
+            '.vsd',
+            '.vsdx',
+            '.wps',
+            '.xls',
+            '.xlsx'
+        ],
+        application: [
+            '.apk',
+            '.bat',
+            '.com',
+            '.deb',
+            '.dll',
+            '.dmg',
+            '.exe',
+            '.ipa',
+            '.jar',
+            '.rpm',
+            '.sh'
+        ],
+        archive: [
+            '.7z',
+            '.arj',
+            '.bz2',
+            '.cab',
+            '.gz',
+            '.iso',
+            '.r',
+            '.rar',
+            '.tar',
+            '.tar',
+            '.z',
+            '.zip'
+        ]
+    });
+}());
