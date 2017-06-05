1db05cf2b339f9eed1b2c6c0a104d20965edcbe6
xujun <xu.jun@99cloud.net>
2017-06-05 21:11:11 +0800

    fix chrome ext

--- src/scripts/core/app.js
+++ src/scripts/core/app.js
@@ -20,5 +20,14 @@
         'angularPromiseButtons',
         'oitozero.ngSweetAlert',
         angularDragula(angular)
+    ])
+    .config([
+        '$compileProvider',
+        function( $compileProvider )
+        {
+            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+            // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+        }
     ]);
+
 }());
