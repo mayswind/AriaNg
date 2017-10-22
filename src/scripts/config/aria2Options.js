007913b1e3dda6c54cea311779ab39fbeeb0530f
MaysWind <i@mayswind.net>
2017-10-22 17:09:25 +0800

    aria2 settings supports aria2 v1.33.0 (docs 450b4f467f9551a81ad6685129f27bb2c9899b58, https://github.com/aria2/aria2/commits/master/doc/manual-src/en/aria2c.rst)

--- src/scripts/config/aria2Options.js
+++ src/scripts/config/aria2Options.js
@@ -316,6 +316,12 @@
             defaultValue: 'true',
             required: true
         },
+        'bt-load-saved-metadata': {
+            since: '1.33.0',
+            type: 'boolean',
+            defaultValue: 'false',
+            required: true
+        },
         'bt-max-open-files': {
             type: 'integer',
             defaultValue: '100',
@@ -468,6 +474,12 @@
             type: 'string',
             readonly: true,
             defaultValue: 'A2-$MAJOR-$MINOR-$PATCH-'
+        },
+        'peer-agent': {
+            since: '1.33.0',
+            type: 'string',
+            defaultValue: 'aria2/$MAJOR.$MINOR.$PATCH',
+            readonly: true
         },
         'seed-ratio': {
             type: 'float',
@@ -839,13 +851,13 @@
         ],
         btOptions: [
             'bt-detach-seed-only', 'bt-enable-hook-after-hash-check', 'bt-enable-lpd', 'bt-exclude-tracker',
-            'bt-external-ip', 'bt-force-encryption', 'bt-hash-check-seed', 'bt-max-open-files', 'bt-max-peers',
+            'bt-external-ip', 'bt-force-encryption', 'bt-hash-check-seed', 'bt-load-saved-metadata', 'bt-max-open-files', 'bt-max-peers',
             'bt-metadata-only', 'bt-min-crypto-level', 'bt-prioritize-piece', 'bt-remove-unselected-file',
             'bt-require-crypto', 'bt-request-peer-speed-limit', 'bt-save-metadata', 'bt-seed-unverified',
             'bt-stop-timeout', 'bt-tracker', 'bt-tracker-connect-timeout', 'bt-tracker-interval', 'bt-tracker-timeout',
             'dht-file-path', 'dht-file-path6', 'dht-listen-port', 'dht-message-timeout', 'enable-dht', 'enable-dht6',
             'enable-peer-exchange', 'follow-torrent', 'listen-port', 'max-overall-upload-limit', 'max-upload-limit',
-            'peer-id-prefix', 'seed-ratio', 'seed-time'
+            'peer-id-prefix', 'peer-agent', 'seed-ratio', 'seed-time'
         ],
         metalinkOptions: [
             'follow-metalink', 'metalink-base-uri', 'metalink-language', 'metalink-location', 'metalink-os',
