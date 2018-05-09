ee1ea1626c83dd2657554be71733be3a798e4982
MaysWind <i@mayswind.net>
2018-05-10 00:42:09 +0800

    fix cannot open torrent file sometimes

--- src/scripts/services/ariaNgFileService.js
+++ src/scripts/services/ariaNgFileService.js
@@ -46,7 +46,7 @@
         };
 
         return {
-            openFileContent: function (fileFilter, successCallback, errorCallback) {
+            openFileContent: function (fileFilter, successCallback, errorCallback, element) {
                 if (!isSupportFileReader) {
                     if (errorCallback) {
                         errorCallback('Your browser does not support loading file!');
@@ -57,43 +57,51 @@
 
                 var allowedExtensions = getAllowedExtensions(fileFilter);
 
-                angular.element('<input type="file" style="display: none"/>').change(function () {
-                    if (!this.files || this.files.length < 1) {
-                        return;
-                    }
+                if (!element || !element.change) {
+                    element = angular.element('<input type="file" style="display: none"/>');
+                }
 
-                    var file = this.files[0];
-                    var fileName = file.name;
-
-                    if (!checkFileExtension(fileName, allowedExtensions)) {
-                        if (errorCallback) {
-                            errorCallback('The selected file type is invalid!');
+                if (element.attr('data-ariang-file-initialized') !== 'true') {
+                    element.change(function () {
+                        if (!this.files || this.files.length < 1) {
+                            return;
                         }
 
-                        return;
-                    }
+                        var file = this.files[0];
+                        var fileName = file.name;
 
-                    var reader = new FileReader();
+                        if (!checkFileExtension(fileName, allowedExtensions)) {
+                            if (errorCallback) {
+                                errorCallback('The selected file type is invalid!');
+                            }
 
-                    reader.onload = function () {
-                        var result = {
-                            fileName: fileName,
-                            base64Content: this.result.replace(/.*?base64,/, '')
+                            return;
+                        }
+
+                        var reader = new FileReader();
+
+                        reader.onload = function () {
+                            var result = {
+                                fileName: fileName,
+                                base64Content: this.result.replace(/.*?base64,/, '')
+                            };
+
+                            if (successCallback) {
+                                successCallback(result);
+                            }
                         };
 
-                        if (successCallback) {
-                            successCallback(result);
-                        }
-                    };
+                        reader.onerror = function () {
+                            if (errorCallback) {
+                                errorCallback('Failed to load file!');
+                            }
+                        };
 
-                    reader.onerror = function () {
-                        if (errorCallback) {
-                            errorCallback('Failed to load file!');
-                        }
-                    };
+                        reader.readAsDataURL(file);
+                    }).attr('data-ariang-file-initialized', 'true');
+                }
 
-                    reader.readAsDataURL(file);
-                }).trigger('click');
+                element.trigger('click');
             }
         };
     }]);
