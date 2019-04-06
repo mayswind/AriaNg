5f6998a34899fd2da1f864ec62ac444afeedcf52
MaysWind <i@mayswind.net>
2019-04-06 22:23:09 +0800

    fix cannot copy exported settings in some browser

--- src/scripts/controllers/settings-ariang.js
+++ src/scripts/controllers/settings-ariang.js
@@ -256,7 +256,9 @@
         });
 
         $scope.copyExportSettings = function () {
-            clipboard.copyText($scope.context.exportSettings);
+            clipboard.copyText($scope.context.exportSettings, {
+                container: angular.element('#export-settings-modal')[0]
+            });
             $scope.context.exportSettingsCopied = true;
         };
 
