a500500b9a944430c2809e7220930f297ef3b2bb
MaysWind <i@mayswind.net>
2019-05-25 19:47:52 +0800

    copy downlaod link and magnet link supports multi tasks

--- src/scripts/controllers/main.js
+++ src/scripts/controllers/main.js
@@ -69,24 +69,36 @@
             return $rootScope.taskContext.getSelectedTaskIds().length > 0;
         };
 
-        $scope.isSingleUrlTaskSelected = function () {
+        $scope.isSelectedTasksAllHaveUrl = function () {
             var selectedTasks = $rootScope.taskContext.getSelectedTasks();
 
-            if (selectedTasks.length !== 1) {
+            if (selectedTasks.length < 1) {
                 return false;
             }
 
-            return !!selectedTasks[0].singleUrl;
+            for (var i = 0; i < selectedTasks.length; i++) {
+                if (!selectedTasks[i].singleUrl) {
+                    return false;
+                }
+            }
+
+            return true;
         };
 
-        $scope.isSingleBittorrentHasInfoHashTaskSelected = function () {
+        $scope.isSelectedTasksAllHaveInfoHash = function () {
             var selectedTasks = $rootScope.taskContext.getSelectedTasks();
 
-            if (selectedTasks.length !== 1) {
+            if (selectedTasks.length < 1) {
                 return false;
             }
 
-            return !!selectedTasks[0].bittorrent && !!selectedTasks[0].infoHash;
+            for (var i = 0; i < selectedTasks.length; i++) {
+                if (!selectedTasks[i].bittorrent || !selectedTasks[i].infoHash) {
+                    return false;
+                }
+            }
+
+            return true;
         };
 
         $scope.isSpecifiedTaskSelected = function () {
@@ -335,19 +347,37 @@
             $rootScope.taskContext.selectAllCompleted();
         };
 
-        $scope.copySelectedOneTaskDownloadLink = function () {
+        $scope.copySelectedTasksDownloadLink = function () {
             var selectedTasks = $rootScope.taskContext.getSelectedTasks();
+            var result = '';
 
-            if (selectedTasks.length === 1) {
-                clipboard.copyText(selectedTasks[0].singleUrl);
+            for (var i = 0; i < selectedTasks.length; i++) {
+                if (i > 0) {
+                    result += '\n';
+                }
+
+                result += selectedTasks[i].singleUrl;
+            }
+
+            if (result.length > 0) {
+                clipboard.copyText(result);
             }
         };
 
-        $scope.copySelectedOneTaskMagnetLink = function () {
+        $scope.copySelectedTasksMagnetLink = function () {
             var selectedTasks = $rootScope.taskContext.getSelectedTasks();
+            var result = '';
 
-            if (selectedTasks.length === 1) {
-                clipboard.copyText('magnet:?xt=urn:btih:' + selectedTasks[0].infoHash);
+            for (var i = 0; i < selectedTasks.length; i++) {
+                if (i > 0) {
+                    result += '\n';
+                }
+
+                result += 'magnet:?xt=urn:btih:' + selectedTasks[i].infoHash;
+            }
+
+            if (result.length > 0) {
+                clipboard.copyText(result);
             }
         };
 
