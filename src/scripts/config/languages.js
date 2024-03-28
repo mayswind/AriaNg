5d7a65d7f2e2e8717313e7443597b5bdb841a8b3
Alessandro Saglia <ale.saglia@gmail.com>
2024-03-28 21:00:19 +0100

    Added italian language

--- src/scripts/config/languages.js
+++ src/scripts/config/languages.js
@@ -15,6 +15,11 @@
             name: 'Traditional Chinese',
             displayName: '繁體中文',
             aliases: ['zh_CHT', 'zh_TW', 'zh_HK', 'zh_MO']
+        },
+        'it_IT': {
+            name: 'Italian',
+            displayName: 'Italiano',
+            aliases: ['it_IT']
         }
     });
 }());
